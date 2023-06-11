d4aa7b1096d0a8637a05692aa7fc97eea7663813
Ayush Sharma <ayush2162020@gmail.com>
2023-06-11 23:40:57 +0530

    feat: dockerized all services

--- dashboard-server/src/index.ts
+++ dashboard-server/src/index.ts
@@ -23,13 +23,17 @@ app.get("/", (req, res) => {
     res.send('Server is live 🍵');
 });
 
-app.post('/get-info', authMiddleware, (req, res) => {
+app.post('/git-info', authMiddleware, (req, res) => {
     const data  = req.body;
     messages.push(data);
 
     fs.writeFileSync(path.join(__dirname, 'data/messages.json'), JSON.stringify(messages, null, 4));
 
     res.sendStatus(200);
+})
+
+app.get('/get-git-info', (req, res) => {
+    res.status(200).json(messages)
 })
 
 app.listen(PORT, () => {
